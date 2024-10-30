5b130b8111e8d87951eb52656c661cd9f92bb8af
NLaundry <me@nathanlaundry.com>
2024-10-30 17:14:43 -0400

    Update setup-zfsbootmenu.sh

--- setup-zfsbootmenu.sh
+++ setup-zfsbootmenu.sh
@@ -20,10 +20,12 @@ ID=$(source /etc/os-release && echo "$ID")  # Get OS ID from /etc/os-release
 
 # Functions
 generate_hostid() {
+  echo "Generating host ID..."
   zgenhostid -f 0x00bab10c
 }
 
 configure_apt_sources() {
+  echo "Configuring APT sources..."
   cat > /etc/apt/sources.list <<EOF
 deb http://deb.debian.org/debian bookworm main contrib non-free-firmware
 deb-src http://deb.debian.org/debian bookworm main contrib non-free-firmware
@@ -40,12 +42,14 @@ EOF
 }
 
 partition_disk() {
+  echo "Partitioning disk $POOL_DISK..."
   sgdisk --zap-all $POOL_DISK
   sgdisk -n1:1M:+512M -t1:EF00 $BOOT_DISK
   sgdisk -n2:0:-10M -t2:BF00 $POOL_DISK
 }
 
 create_zpool() {
+  echo "Creating ZFS pool and datasets..."
   zpool create -f -o ashift=12 -O compression=lz4 -O acltype=posixacl -O xattr=sa -O relatime=on -o autotrim=on -o compatibility=openzfs-2.1-linux -m none $POOL_NAME ${POOL_DISK}p${POOL_PART}
   zfs create -o mountpoint=none $POOL_NAME/ROOT
   zfs create -o mountpoint=/ -o canmount=noauto $POOL_NAME/ROOT/$ID
@@ -54,6 +58,7 @@ create_zpool() {
 }
 
 export_import_zpool() {
+  echo "Exporting and re-importing ZFS pool for mounting..."
   zpool export $POOL_NAME
   zpool import -N -R $MOUNT_POINT $POOL_NAME
   zfs mount $POOL_NAME/ROOT/$ID
@@ -61,12 +66,14 @@ export_import_zpool() {
 }
 
 setup_base_system() {
+  echo "Installing base system with debootstrap..."
   debootstrap bookworm $MOUNT_POINT
   cp /etc/hostid $MOUNT_POINT/etc/hostid
   cp /etc/resolv.conf $MOUNT_POINT/etc/resolv.conf
 }
 
 prepare_chroot() {
+  echo "Mounting filesystems for chroot environment..."
   mount --bind /dev $MOUNT_POINT/dev
   mount --bind /proc $MOUNT_POINT/proc
   mount --bind /sys $MOUNT_POINT/sys
@@ -74,6 +81,7 @@ prepare_chroot() {
 }
 
 enter_chroot() {
+  echo "Entering chroot environment to configure system..."
   chroot $MOUNT_POINT /bin/bash <<EOF
 # Set hostname
 echo "$HOSTNAME" > /etc/hostname
@@ -87,19 +95,31 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y locales linux-headers-$KERNEL_VERSION linux-image-amd64 zfs-initramfs dosfstools efibootmgr curl
 
-# Configure locale and timezone
+# Install system utilities
+echo "Installing system utilities..."
+apt install -y systemd-timesyncd net-tools iproute2 isc-dhcp-client iputils-ping traceroute curl wget dnsutils ethtool ifupdown tcpdump nmap nano vim htop openssh-server git tmux
+
+# Perform system upgrade
+echo "Upgrading system packages..."
+apt upgrade -y
+
+# Set locale and timezone
+echo "Configuring locale and timezone..."
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 dpkg-reconfigure -f noninteractive tzdata
 
 # Set root password
+echo "Setting root password..."
 echo "root:$ROOT_PASSWORD" | chpasswd
 
 # Create user and set password
-useradd -m -s /bin/bash -G sudo $USERNAME
+echo "Creating user and setting permissions..."
+useradd -m -s /bin/bash -G sudo,audio,cdrom,dip,floppy,netdev,plugdev,video $USERNAME
 echo "$USERNAME:$USER_PASSWORD" | chpasswd
 
 # Configure ZFSBootMenu
+echo "Setting up ZFSBootMenu EFI configuration..."
 mkdir -p /boot/efi/EFI/ZBM
 curl -Lo /boot/efi/EFI/ZBM/VMLINUZ.EFI https://get.zfsbootmenu.org/efi
 
@@ -109,12 +129,19 @@ mkdir -p /boot/efi
 mount ${BOOT_DISK}p${BOOT_PART} /boot/efi
 
 # Set up EFI boot entries
+echo "Adding EFI boot entries..."
 efibootmgr -c -d ${BOOT_DISK} -p $BOOT_PART -L "ZFSBootMenu" -l '\\EFI\\ZBM\\VMLINUZ.EFI'
 efibootmgr -c -d ${BOOT_DISK} -p $BOOT_PART -L "ZFSBootMenu (Backup)" -l '\\EFI\\ZBM\\VMLINUZ-BACKUP.EFI'
+efibootmgr -c -d ${BOOT_DISK} -p $BOOT_PART -L "ZFSBootMenu" -l '\\EFI\\BOOT\\bootx64.efi'
+
+# Perform a distribution upgrade
+echo "Running dist-upgrade to upgrade all packages to the latest version..."
+apt full-upgrade -y
 EOF
 }
 
 cleanup_chroot() {
+  echo "Cleaning up chroot environment..."
   umount -l $MOUNT_POINT/dev/pts
   umount -l $MOUNT_POINT/dev
   umount -l $MOUNT_POINT/sys
@@ -122,10 +149,12 @@ cleanup_chroot() {
 }
 
 final_cleanup() {
+  echo "Exporting ZFS pool and completing installation..."
   zpool export -a
 }
 
 # Execution sequence
+echo "Starting ZFS Boot Menu installation..."
 generate_hostid
 configure_apt_sources
 apt update
